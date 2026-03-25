d0ce460f8edd281d8cd14fe60ce1488e93021ecc
bdawit1 <bdawit2005@gmail.com>
2026-03-25 10:40:06 -0400

    Clean up Movable interface documentation
    Removed unnecessary project header information and an author.

--- SnakeGame/src/snake/Moveable.java
+++ SnakeGame/src/snake/Moveable.java
@@ -4,9 +4,7 @@ package snake;
  * Movable
  * <p>Interface for objects that advance one step per tick.</p>
  *
- * <h2>Project:</h2> CMSC132 – Project 4 (Snake)
  * @author Brook Dawit
- * @author Moshe
  * @version 1.0
  * @since 2025-10-17
  */
